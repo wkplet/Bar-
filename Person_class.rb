b119f010336ce148801d55e76262b169e6eda92d
tadahmen <tadahmen@gmail.com>
2016-02-04 15:17:53 +0100

    1st version

---
+++ Person_class.rb
@@ -0,0 +1,22 @@
+class Person
+  def initialize(name)
+    @name = name
+  end
+​
+  def greet(other_person)
+    say "Hi, how are ya?"
+    other_person.greet_back
+  end
+​
+  def greet_back
+    say "Fine"
+  end
+​
+  def what_is_your_name
+    say @name
+  end
+​
+  def say(message)
+    puts "#{@name}: #{message}!"
+  end
+end
